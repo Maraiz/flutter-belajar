09b6311a455e375947eeb18cdf04fac959ecb71b
Maraiz <Anakbudiman01@gmail.com>
2023-10-07 14:25:58 +0700

    ubah nama

--- login_register/lib/main.dart
+++ login_register/lib/main.dart
@@ -106,7 +106,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'parah',
+              'iyahaka',
             ),
             Text(
               '$_counter',
