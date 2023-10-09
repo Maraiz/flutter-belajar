bdd84f18f081cf67c1a7a1a1ec939bfbe50456ec
Maraiz <Anakbudiman01@gmail.com>
2023-10-09 09:09:08 +0700

    yes bisa

--- login_register/lib/theme.dart
+++ login_register/lib/theme.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:login_register/utils/constants.dart';
- 
+
 ThemeData theme() {
   return ThemeData(
     scaffoldBackgroundColor: Colors.white,
@@ -11,7 +11,7 @@ ThemeData theme() {
     visualDensity: VisualDensity.adaptivePlatformDensity,
   );
 }
- 
+
 InputDecorationTheme inputDecorationTheme() {
   OutlineInputBorder outlineInputBorder = OutlineInputBorder(
     borderRadius: BorderRadius.circular(28),
@@ -19,9 +19,6 @@ InputDecorationTheme inputDecorationTheme() {
     gapPadding: 10,
   );
   return InputDecorationTheme(
-    // If  you are using latest version of flutter then lable text and hint text shown like this
-    // if you r using flutter less then 1.20.* then maybe this is not working properly
-    // if we are define our floatingLabelBehavior in our theme then it's not applayed
     floatingLabelBehavior: FloatingLabelBehavior.always,
     contentPadding: EdgeInsets.symmetric(horizontal: 42, vertical: 20),
     enabledBorder: outlineInputBorder,
@@ -29,23 +26,22 @@ InputDecorationTheme inputDecorationTheme() {
     border: outlineInputBorder,
   );
 }
- 
+
 TextTheme textTheme() {
-  return const TextTheme(
+  return TextTheme(
     bodyText1: TextStyle(color: kTextColor),
     bodyText2: TextStyle(color: kTextColor),
   );
 }
- 
+
 AppBarTheme appBarTheme() {
-  return const AppBarTheme(
+  return AppBarTheme(
     color: Colors.white,
     elevation: 0,
-    brightness: Brightness.light,
-    iconTheme: IconThemeData(color: Colors.black),
-    textTheme: TextTheme(
+    iconTheme: IconThemeData(color: Colors.black), toolbarTextStyle: TextTheme(
       headline6: TextStyle(color: Color(0XFF8B8B8B), fontSize: 18),
-    ),
+    ).bodyText2, titleTextStyle: TextTheme(
+      headline6: TextStyle(color: Color(0XFF8B8B8B), fontSize: 18),
+    ).headline6,
   );
 }
- 
